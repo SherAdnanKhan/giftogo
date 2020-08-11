72f2a0f0f14f4194645d7fa522e4e8893456b276
qureshius <khurram@codility.co>
2020-08-11 15:28:50 +0500

    featuring sequelize

--- app.js
+++ app.js
@@ -1,7 +1,41 @@
+require("dotenv").config();
 const express = require("express");
-const app = express();
+const logger = require("morgan");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const createError = require("http-errors");
 const port = parseInt(process.env.PORT, 10) || 8080;
 
-app.listen(port, () => {
-  console.log(`Server started on port ${port}`);
+// Set up the express app
+const app = express();
+app.use(cors());
+// Log requests to the console.
+app.use(logger("dev"));
+
+// Parse incoming requests data (https://github.com/expressjs/body-parser)
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+const model = require("./models");
+
+app.use(function(req, res, next) {
+  next(createError(404))
+})
+
+// error handler
+app.use(function (err, req, res, _next) {
+  console.log("===ERORR====", err);
+  res
+    .status(err.status || 500)
+    .send({ error: err.message ? err.message : "internal server error" });
 });
+
+model.sequelize
+  .sync({
+    force: false,
+    //alter: true // please do not remove this alter
+  })
+  .then((connection) => {
+    app.listen(port, () => {
+      console.log(`Server started on ${port}`);
+    });
+  });
