229fe2c97544b58347111da940334b06a343417b
Khurram Qureshi <khurram@codility.co>
2020-09-07 10:15:35 +0500

    Change in response

--- responses/index.js
+++ responses/index.js
@@ -2,13 +2,13 @@
 const generateResponse = (body, code, event) => {
   console.log('Generate Response Function');
   return {
-    body: JSON.stringify(body),
-    statusCode: code,
-    headers: {
-      'Content-Type': 'application/json',
-      'Access-Control-Allow-Origin': '*',
-      // ...cors.corsHeaders(event),
-    },
+    result: body,
+    status: code,
+    // headers: {
+    //   'Content-Type': 'application/json',
+    //   'Access-Control-Allow-Origin': '*',
+    //   // ...cors.corsHeaders(event),
+    // },
   };
 };
 
