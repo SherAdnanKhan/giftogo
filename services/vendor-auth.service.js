f24a6b0e01517700cec02287d28067c08f784284
Khurram Qureshi <khurram@codility.co>
2020-09-15 12:23:27 +0500

    Fixes vendor forgot password

--- services/vendor-auth.service.js
+++ services/vendor-auth.service.js
@@ -9,8 +9,9 @@ const shopify = require("../lib/shopify");
 const { Vendor } = require("../models");
 
 const { error } = require("../errors");
-const smtp=require('./email.service');
+const smtp = require('./email.service');
 const { realpathSync } = require("fs");
+const { sendemail } = require("./email.service");
 
 // login user service
 const loginVendor = async (_vendor) => {
@@ -75,10 +76,10 @@ const createVendor = async (_vendor) => {
         shopify_collection_id: shopifyCollection.id
       });
 
-      const textbody="<html><head><style>.button{background-color: #008CBA;border: none;color: white;padding: 15px 32px;text-align: center;text-decoration: none;display: inline-block;font-size: 16px;margin: 4px 2px;cursor: pointer;}.im {color: black;}</style></head>"
-      +"<body><h1>Giftogo</h1><h1>Welcome to Giftogo!</h1><p>You have activated your business account. Next time to login.</p><a class="+"button button2"+" href="+"https://giftogo.co/pages/business-login"+">Visit your store</a></body></html>"
+      const textbody = "<html><head><style>.button{background-color: #008CBA;border: none;color: white;padding: 15px 32px;text-align: center;text-decoration: none;display: inline-block;font-size: 16px;margin: 4px 2px;cursor: pointer;}.im {color: black;}</style></head>"
+        + "<body><h1>Giftogo</h1><h1>Welcome to Giftogo!</h1><p>You have activated your business account. Next time to login.</p><a class=" + "button button2" + " href=" + "https://giftogo.co/pages/business-login" + ">Visit your store</a></body></html>"
 
-      let emailinfo= await smtp.sendemail(textbody,email,"Giftogo Business account registration");
+      let emailinfo = await smtp.sendemail(textbody, email, "Giftogo Business account registration");
 
       console.log(emailinfo);
 
@@ -94,39 +95,70 @@ const createVendor = async (_vendor) => {
   }
 };
 
-const forgotVendor= async (data)=>{
-    const {email}=data;
-    let vendor = await Vendor.findOne({ where: { email }, limit: 1 });
-    if (!vendor) {
-      return { message: "Invalid Email", response: [], status: 400 }
-    }
+const forgotVendor = async (data) => {
+  const { email } = data;
+  let vendor = await Vendor.findOne({ where: { email }, limit: 1 });
+  if (!vendor) {
+    return { message: "Invalid Email", response: [], status: 400 }
+  }
 
-    // var token = new Token({ _userId: result._id, token: crypto.randomBytes(16).toString('hex') });
-    // console.log(vendor,crypto.randomBytes(16).toString('hex'));
+  // var token = new Token({ _userId: result._id, token: crypto.randomBytes(16).toString('hex') });
+  // console.log(vendor,crypto.randomBytes(16).toString('hex'));
+  try {
+    cryptotoken = crypto.randomBytes(16).toString('hex');
 
-    cryptotoken=crypto.randomBytes(16).toString('hex');
-
-    const newvendor=await Vendor.update(
-      { reset_token:cryptotoken},
-      { where: {id: vendor.id } }
+    const newvendor = await Vendor.update(
+      { reset_token: cryptotoken },
+      { where: { id: vendor.id } }
     )
 
-    console.log(newvendor);
+    const htmlbody = `<html>
+                        <head>
+                          <style>
+                            .button{
+                              background-color: #008CBA;
+                              border: none;color: white;
+                              padding: 15px 32px;
+                              text-align: center;
+                              text-decoration: none;
+                              display: inline-block;
+                              font-size: 16px;
+                              margin: 4px 2px;
+                              cursor: pointer;
+                            }
+                            .im{
+                              color: black;
+                            }
+                            a{
+                              text-decoration:none;
+                              color:#008CBA
+                            }
+                          </style>
+                        </head>
+                        <body>
+                          <h1>Giftogo</h1>
+                          <h1>Reset your password</h1>
+                          <p>Follow this link to reset your Partner account password at 
+                            <a href="https://giftogo.co/">Giftogo</a>.
+                            If you didn't request a new password, you can safely delete this email.
+                          </p>
+                          <a class="button" href="">Reset your password</a>
+                            or 
+                          <a href="https://giftogo.co/pages/business-login">Visit your store</a>
+                        </body>
+                      </html>`;
+    await smtp.sendemail(htmlbody, email, "Giftogo Reset Password");
+  } catch (e) {
+    console.log(e);
+    return { message: "Email Could not be sent this time.", response: [], status: 400 }
+  }
 
-    const htmlbody="<html><head><style>.button{background-color: #008CBA;border: none;color: white;padding: 15px 32px;text-align: center;text-decoration: none;display: inline-block;font-size: 16px;margin: 4px 2px;cursor: pointer;}.im{color: black;}a{text-decoration:none;color:#008CBA}</style></head><body><h1>Giftogo</h1><h1>Reset your password</h1><p>Follow this link to reset your Partner account password at <a href="+"https://giftogo.co/"+">Giftogo</a>. If you didn't request a new password, you can safely delete this email.</p><a class="+"button button2"+" href="+">Reset your password</a> or <a href="+"https://giftogo.co/pages/business-login"+">Visit your store</a></body></html>";
-    
-    let emailinfo= await smtp.sendemail(htmlbody,email,"Giftogo Reset Password");
-
-    console.log(emailinfo);
-    
-
-    return {message:"Email sent please check your email.", token:cryptotoken}
-    
+  return { message: "Email sent please check your email.", token: cryptotoken, status: 200 }
 }
 
-const resetPasswordVendor= async (data)=>{
-  
-  const {email,password,token}=data;
+const resetPasswordVendor = async (data) => {
+
+  const { email, password, token } = data;
 
   let vendor = await Vendor.findOne({ where: { email }, limit: 1 });
   if (!vendor) {
@@ -135,22 +167,23 @@ const resetPasswordVendor= async (data)=>{
 
   console.log(vendor);
   var newvendor;
-  if(vendor.reset_token===token){
+  if (vendor.reset_token === token) {
     const salt = await bcrypt.genSalt(10);
     const hash_password = await bcrypt.hash(password, salt);
-    newvendor=await Vendor.update(
-      { reset_token:"",
+    newvendor = await Vendor.update(
+      {
+        reset_token: "",
         password: hash_password,
       },
-      { where: {id: vendor.id } }
+      { where: { id: vendor.id } }
     )
   }
-  else{
-    return {message: "Token expired", response: [], status: 400}
+  else {
+    return { message: "Token expired", response: [], status: 400 }
   }
 
-  return {message:"Password Reset Success",response: newvendor, status: 200}
-  
+  return { message: "Password Reset Success", response: newvendor, status: 200 }
+
 }
 
 module.exports = {
@@ -158,5 +191,5 @@ module.exports = {
   loginVendor,
   forgotVendor,
   resetPasswordVendor
-  
+
 };
