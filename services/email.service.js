6b98d3fea983e606f1c02f21eb86f0d4b0d96f2d
Khurram Qureshi <khurram@codility.co>
2020-09-16 18:09:16 +0500

    Business number added + active link email

--- services/email.service.js
+++ services/email.service.js
@@ -1,37 +1,37 @@
 var nodemailer = require('nodemailer');
 
 var transporter = nodemailer.createTransport({
-    host: 'server148.web-hosting.com', 
-    port: 465, 
+    host: process.env.EMAIL_SMTP_HOST,
+    port: process.env.EMAIL_SMTP_PORT,
     auth: {
-        user: 'no-reply@giftogo.co',
-        pass: 'giftogodev!@#$'
+        user: process.env.EMAIL_SMTP_USER,
+        pass: process.env.EMAIL_SMTP_PASS
     }
 });
 
 
 
-const sendemail=async (bodyhtml,emailto,subject)=>{
+const sendemail = async (bodyhtml, emailto, subject) => {
 
     var mailOptions = {
         from: 'no-reply@giftogo.co', //It will work if i give me@myserver.com but i need no-reply@myserver.com in from option.        
         to: emailto,
         subject: subject,
         // text: bodytext,
-        html:bodyhtml,
+        html: bodyhtml,
     };
 
-    transporter.sendMail(mailOptions, function(error, info){
+    transporter.sendMail(mailOptions, function (error, info) {
         if (error) {
-         console.log(error);
+            console.log(error);
         } else {
-         console.log('Email sent: ' + info.response);
+            console.log('Email sent: ' + info.response);
         }
     });
 }
 
-module.exports={
+module.exports = {
     sendemail
 }
 
- 
+
