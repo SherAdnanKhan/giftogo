f17ce8f2894006da287c49f66bf1a0905558385b
Khurram Qureshi <khurram@codility.co>
2020-09-29 20:04:20 +0500

    Tango card servies against order

--- routes/order.routes.js
+++ routes/order.routes.js
@@ -4,6 +4,7 @@ const orderListRouter = express.Router();
 const ordersContorller = require("../controller/orders.controller");
 
 orderListRouter.get("/order/:id", ordersContorller.masking);
+orderListRouter.get("/order/:orderId/:customerId", ordersContorller.getOrderBarCode);
 
 
 module.exports = orderListRouter;
